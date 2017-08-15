0ded30cd71bfae920480f975deb5a97d944932e0
Paul Price <price@astro.princeton.edu>
2017-08-15 11:47:17 -0700

    remove swig as a dependency
    We're using pybind11 now, and don't want swig any more.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,6 +1,6 @@
 build()
 {
-	(mkdir build && cd build && cmake -DNDARRAY_SWIG=ON -DNDARRAY_PYBIND11=ON -DFFTW_ROOT=$FFTW_DIR -DBOOST_ROOT=$BOOST_DIR -DCMAKE_INSTALL_PREFIX=$PREFIX .. && make && make test)
+	(mkdir build && cd build && cmake -DNDARRAY_SWIG=OFF -DNDARRAY_PYBIND11=ON -DFFTW_ROOT=$FFTW_DIR -DBOOST_ROOT=$BOOST_DIR -DCMAKE_INSTALL_PREFIX=$PREFIX .. && make && make test)
 }
 
 install()
