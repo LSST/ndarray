72df944fc1dd8c75ac126ed89e3a7ede2b5406d2
Russell Owen <rowen@uw.edu>
2018-06-15 08:39:18 -0700

    ndarray version 1.5.1
    Include free functions `asEigen`, `asEigenArray` and `asEigenMatrix`.
    This version of ndarray also allows building without EigenView,
    for compatibility with Eigen 3.3. But switching to that is left
    for another ticket, as it will require some changes to our pybind11
    wrappers.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,6 +1,6 @@
 build()
 {
-	(mkdir build && cd build && cmake -DNDARRAY_SWIG=OFF -DNDARRAY_PYBIND11=ON -DFFTW_ROOT=$FFTW_DIR -DBOOST_ROOT=$BOOST_DIR -DEIGEN3_INCLUDE_DIR=$EIGEN_DIR/include -DCMAKE_INSTALL_PREFIX=$PREFIX .. && make && make test)
+	(mkdir build && cd build && cmake -DNDARRAY_PYBIND11=ON -DNDARRAY_EIGENVIEW=ON -DFFTW_ROOT=$FFTW_DIR -DBOOST_ROOT=$BOOST_DIR -DEIGEN3_INCLUDE_DIR=$EIGEN_DIR/include -DCMAKE_INSTALL_PREFIX=$PREFIX .. && make && make test)
 }
 
 install()
